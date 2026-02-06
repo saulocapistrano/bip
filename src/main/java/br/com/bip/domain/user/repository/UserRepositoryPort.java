8b0294e577cd630a8ddfed710b527bb409f5eb3d
saulocapistrano <saulocapistrano@gmail.com>
2026-02-06 15:59:45 -0300

    feat: cria porta de repositorio de usuario

---
+++ src/main/java/br/com/bip/domain/user/repository/UserRepositoryPort.java
@@ -0,0 +1,19 @@
+package br.com.bip.domain.user.repository;
+
+import br.com.bip.domain.user.model.User;
+import br.com.bip.domain.user.model.UserRole;
+import br.com.bip.domain.user.model.UserStatus;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepositoryPort {
+
+    User save(User user);
+    Optional<User> findById(UUID id);
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+    List<User> findByRoleStatus(UserRole role, UserStatus status);
+
+}
